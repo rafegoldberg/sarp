a587c7b8439287b731042fd2bbcbdcd4b13abb7a
Rafe Goldberg <rafegoldberg@gmail.com>
2017-01-24 13:51:01 -0800

    better comments

--- .env/conf/ui.css-tasks.js
+++ .env/conf/ui.css-tasks.js
@@ -1,39 +1,46 @@
 var
 tasks={
 	sass_globbing:{
-    files: {
-      '<%=ƒ.sass.temp%>':[
-	      '<%=ƒ.sass.main%>',
-	      '<%=ƒ.sass.core%>/config/*.scss',
-	      '<%=ƒ.sass.core%>/{base,generic,layout}/*.scss',
-	      '<%=ƒ.sass.core%>/extends/*.scss',
-	      '<%=ƒ.sass.atom%>',
-	      ],
-	    //=test
-	     //  'UI.CORE.CONF.scss':[
-		    //   '<%=ƒ.sass.main%>',
-		    //   '<%=ƒ.sass.core%>/config/*.scss',
-		    //   ],
-	     //  'UI.CORE.XTRA.scss':[
-		    //   '<%=ƒ.sass.core%>/{base,generic,layout}/*.scss',
-		    //   '<%=ƒ.sass.core%>/extends/*.scss',
-		    //   ],
-	     //  'UI.CORE.scss':[
-		    //   'UI.CORE.CONF.scss',
-		    //   'UI.CORE.XTRA.scss',
-		    //   ],
-	     //  'UI.ATOM.scss':[
-		    //   '<%=ƒ.sass.atom%>'],
-		    // '<%=ƒ.sass.temp%>':[//'UI.GLOB.scss':[
-		    // 	'UI.CORE.scss',
-		    // 	'UI.ATOM.scss'
-			   //  ],
-      },
-    options: {
-      useSingleQuotes: true,
-      signature: '//=Ui Sass Glob'
-      }
-    },
+		files: {
+			'<%=ƒ.sass.temp%>':[
+				'<%=ƒ.sass.main%>',
+				'<%=ƒ.sass.core%>/config/*.scss',
+				'<%=ƒ.sass.core%>/base/*.scss',
+				'<%=ƒ.sass.core%>/generic/*.scss',
+				'<%=ƒ.sass.core%>/layout/*.scss',
+				// '<%=ƒ.sass.core%>/{base,generic,layout}/*.scss',
+				'<%=ƒ.sass.core%>/extends/*.scss',
+				'<%=ƒ.sass.atom%>',
+				],
+			/*\
+			 @refactor[individual glob exports]
+			 @fix[sourcemaps]
+			 |->
+				'UI.CORE.CONF.scss':[
+					'<%=ƒ.sass.main%>',
+					'<%=ƒ.sass.core%>/config/*.scss',
+					],
+				'UI.CORE.XTRA.scss':[
+					'<%=ƒ.sass.core%>/{base,generic,layout}/*.scss',
+					'<%=ƒ.sass.core%>/extends/*.scss',
+					],
+				'UI.CORE.scss':[
+					'UI.CORE.CONF.scss',
+					'UI.CORE.XTRA.scss',
+					],
+				'UI.ATOM.scss':[
+					'<%=ƒ.sass.atom%>'],
+				'<%=ƒ.sass.temp%>':[//'UI.GLOB.scss':[
+					'UI.CORE.scss',
+					'UI.ATOM.scss'
+					],
+			\*/
+			},
+		options: {
+			useSingleQuotes: true,
+			signature: '//=Ui Sass Glob'
+			}
+		},
 	sass:{
 		options: {
 			sourceMap: true,
